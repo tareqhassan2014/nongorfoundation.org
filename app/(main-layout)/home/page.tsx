c6547abfa8e30b3c1966e437323dffc229137b82
Naimur <m.naimur61@gmail.com>
2024-03-10 20:26:29 +0600

    header done

--- app/(main-layout)/home/page.tsx
+++ app/(main-layout)/home/page.tsx
@@ -1,14 +1,14 @@
-import BrowseCategory from '@/components/BrowseByCategorys/BrowseCategory';
-import LatestCauses from '@/components/LatestCauses/LatestCauses';
-import React from 'react';
+import BrowseCategory from "@/components/Home/BrowseByCategory/BrowseCategory";
+import LatestCauses from "@/components/Home/LatestCauses/LatestCauses";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <LatestCauses />
-            <BrowseCategory />
-        </div>
-    );
+	return (
+		<div>
+			<LatestCauses />
+			<BrowseCategory />
+		</div>
+	);
 };
 
 export default Home;
