d84871eaa66794bed92142262c198b39d2ae0e43
Naimur <m.naimur61@gmail.com>
2024-03-12 15:46:36 +0600

    [Change] Navbar logo

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/logo-color.png";
+import logo from "@/public/Profile Photo logo.jpg";
 
 interface MenuItem {
 	name: string;
