28d44db1888407951b75ffb3beef9daea84cf32b
Naimur <m.naimur61@gmail.com>
2024-03-13 17:20:55 +0600

    [Done] navbar responsive

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/Nogor Foundation.jpg";
 import { Button } from "../ui/button";
+import { GiCancel } from "react-icons/gi";
 
 interface MenuItem {
 	name: string;
@@ -60,8 +61,23 @@ const Navbar = () => {
 	return (
 		<div className="navbar my-3">
 			<div className="navbar-start">
-				<div className="dropdown">
-					<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+				<Image src={logo} height={90} width={150} className="md:w-56" alt="website logo" />
+			</div>
+			{/* Menu */}
+			<div className="navbar-center hidden lg:flex">
+				<ul className="menu menu-horizontal px-1 flex">{GenerateMenu(Menu)}</ul>
+			</div>
+			<div className=" navbar-end">
+				{/* Navbar Drawer  */}
+				<div className="drawer drawer-end">
+					<input id="navbar-drawer" type="checkbox" className="drawer-toggle" />
+					{/* Drawer Button  */}
+					<label
+						tabIndex={0}
+						role="button"
+						htmlFor="navbar-drawer"
+						className="btn btn-ghost lg:hidden  justify-end"
+					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							className="h-5 w-5"
@@ -71,36 +87,30 @@ const Navbar = () => {
 						>
 							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
 						</svg>
+					</label>
+
+					{/* Drawer Content  */}
+					<div className="drawer-side">
+						<label htmlFor="navbar-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+
+						<div className="p-4 w-80 min-h-full bg-base-200 text-base-content">
+							<Link href="/">
+								<Button variant="custom" size={"sm"}>
+									Donate
+								</Button>
+							</Link>
+
+							<ul className="menu menu-vertical mt-5">{GenerateMenu(Menu)}</ul>
+						</div>
 					</div>
-					<ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52">
-						<li>
-							<a>Item 1</a>
-						</li>
-						<li>
-							<a>Parent</a>
-							<ul className="p-2">
-								<li>
-									<a>Submenu 1</a>
-								</li>
-								<li>
-									<a>Submenu 2</a>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<a>Item 3</a>
-						</li>
-					</ul>
 				</div>
-				<Image src={logo} height={90} width={150} className="md:w-56" alt="website logo" />
-			</div>
-			<div className="navbar-center hidden lg:flex">
-				<ul className="menu menu-horizontal px-1 flex">{GenerateMenu(Menu)}</ul>
-			</div>
-			<div className="navbar-end">
-				<Link href={"/"}>
-					<Button variant={"custom"}>Donate</Button>
-				</Link>
+
+				{/* Donate Button */}
+				<div className="hidden lg:block">
+					<Link href={"/"}>
+						<Button variant={"custom"}>Donate</Button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
