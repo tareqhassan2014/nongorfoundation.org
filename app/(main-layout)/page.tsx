36dcfa261e2da4caaa3bb4bce2e731e2d945ddba
Sabithasan2098 <sabithasan2098@gmail.com>
2024-03-13 15:07:17 +0600

    cut the home folder

--- app/(main-layout)/page.tsx
+++ app/(main-layout)/page.tsx
@@ -2,6 +2,7 @@ import BrowseCategory from "@/components/Home/BrowseByCategory/BrowseCategory";
 import LatestCauses from "@/components/Home/LatestCauses/LatestCauses";
 import MedicalCamp from "@/components/Home/MedicelCamp/MedicalCamp";
 import NaturalDisaster from "@/components/Home/NaturalDisaster/NaturalDisaster";
+import EssentialsTriad from "@/components/Home/EssentialsTriad/EssentialsTriad";
 import React from "react";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
 			<LatestCauses />
 			<NaturalDisaster />
 			<MedicalCamp />
+			<EssentialsTriad />
 			<BrowseCategory />
 		</div>
 	);
