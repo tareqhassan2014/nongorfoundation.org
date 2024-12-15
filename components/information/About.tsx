654f7d52e74671a50e2810b0a3ccc71d2e9d6a0b
Sabit Hasan <sabithasan2098@gmail.com>
2024-12-15 17:54:49 +0600

    translate language added

--- components/information/About.tsx
+++ components/information/About.tsx
@@ -9,15 +9,24 @@ import { paths } from "@/utils/paths";
 
 const About: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>(AboutData[0].name);
-  
+
   return (
     <div>
       <Footstep>
-        <Link href={paths.home} className=" cursor-pointer hover:text-green-800 "> হোম </Link> / <span className=" text-green-800 ">আমাদের সম্পর্কে</span>
+        <Link
+          href={paths.home}
+          className=" cursor-pointer hover:text-green-800 "
+        >
+          {" "}
+          হোম{" "}
+        </Link>{" "}
+        / <span className=" text-green-800 ">আমাদের সম্পর্কে</span>
       </Footstep>
       <div className=" py-20 ">
-        <h2 className="text-center py-3 text-2xl font-semibold">আমাদের সম্পর্কে</h2>
-        <div >
+        <h2 className="text-center py-3 text-2xl font-semibold">
+          আমাদের সম্পর্কে
+        </h2>
+        <div>
           <div className="container mx-auto py-10">
             <div className="grid grid-cols-12">
               <Tabs
@@ -37,7 +46,7 @@ const About: React.FC = () => {
                   </div>
                 ))}
               </Tabs>
-              
+
               <section className="lg:col-span-9 col-span-12 lg:border-l lg:px-5 overflow-auto">
                 <div className="bg-white rounded-md p-3">
                   {AboutData.map(
@@ -47,7 +56,9 @@ const About: React.FC = () => {
                           <h1 className=" text-2xl text-green-600 mb-5 ">
                             {data.name}
                           </h1>
-                          <p id={data.name} className=" text-justify ">{data.description}</p>
+                          <p id={data.name} className=" text-justify ">
+                            {data.description}
+                          </p>
                         </div>
                       )
                   )}
